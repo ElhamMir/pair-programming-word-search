75953b7616fdad5dd4c8c92ce70e919a1d23e1df
Elham <mireshghi@yahoo.com>
2021-11-07 23:50:42 +0000

    add more tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,13 @@
 const wordSearch = (letters, word) => { 
+    if(word.length === 0 || letters.length === 0){
+        return false
+    }
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
+        
         if (l.includes(word)) return true
     }
+    return false
 }
 
 module.exports = wordSearch
